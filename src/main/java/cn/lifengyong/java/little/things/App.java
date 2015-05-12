00015501d0f64f2044e148b199d545e05e53b29d
lifengyong <wanshui_qianshan@126.com>
2015-05-12 10:03:37 +0800

    project init

---
+++ src/main/java/cn/lifengyong/java/little/things/App.java
@@ -0,0 +1,13 @@
+package cn.lifengyong.java.little.things;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
